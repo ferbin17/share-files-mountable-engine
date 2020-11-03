c2d26161849aa17dc384b66d9f997c1e3565419c
ferbin <ferbin17@gmail.com>
2020-11-03 08:20:19 +0530

    feat: add manifest to precompile

--- app/assets/config/share_files_app_manifest.js
+++ app/assets/config/share_files_app_manifest.js
@@ -1,1 +1,3 @@
 //= link_directory ../stylesheets/share_files_app .css
+//= link_directory ../javascript/share_files_app .js
+//= link_tree ../images/share_files_app
--- lib/share_files_app/engine.rb
+++ lib/share_files_app/engine.rb
@@ -2,13 +2,8 @@ module ShareFilesApp
   class Engine < ::Rails::Engine
     isolate_namespace ShareFilesApp
 
-    # initializer "engine_name.assets.precompile" do |app|
-    #   app.config.assets.precompile += %w(share_files_app/application.js share_files_app/application.css)
-    # end
-    
-    # Initializer to combine this engines static assets with the static assets of the hosting site.
-   #  initializer "static assets" do |app|
-   #   app.middleware.insert_before(::ActionDispatch::Static, ::ActionDispatch::Static, "#{root}/public")
-   # end
+    initializer "share_files_app.assets.precompile" do |app|
+      app.config.assets.precompile << "share_files_app_manifest.js"
+    end
   end
 end
